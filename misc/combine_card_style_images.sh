2a472d9fa496032633f483560f809dd4f5d27b5a
ADBond <48208438+ADBond@users.noreply.github.com>
2023-05-29 23:01:46 +0100

    layout cards with four-colour (calypso spaces)

--- misc/combine_card_style_images.sh
+++ misc/combine_card_style_images.sh
@@ -6,3 +6,5 @@
 # better to do this less manually, but let's consider that an upgrade
 magick montage misc/img_raw/card_suits_small.png misc/img_raw/card_suits_small_four.png -background none \
     -tile 2x1 -geometry +0+0 img/card_suits.png
+magick montage misc/img_raw/cards_layout.png misc/img_raw/cards_layout_four.png -background none \
+    -tile 2x1 -geometry +0+0 img/cards_layout.png
