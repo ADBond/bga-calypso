1e24cc0ca9b20a58e6aa1a94952b9b7823bee2c9
ADBond <48208438+ADBond@users.noreply.github.com>
2023-05-29 22:30:17 +0100

    card suits (for renounce) four-colour option as extra column

---
+++ misc/combine_card_style_images.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# definitely worthwhile making this _another_ bash script to fill up this folder
+
+# order L->R is same as order in user-prefs
+
+# better to do this less manually, but let's consider that an upgrade
+magick montage misc/img_raw/card_suits_small.png misc/img_raw/card_suits_small_four.png -background none \
+    -tile 2x1 -geometry +0+0 img/card_suits.png
