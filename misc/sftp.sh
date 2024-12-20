b29b826881b7a02ef0ea6d305b78c0fa9427b7aa
ADBond <48208438+ADBond@users.noreply.github.com>
2024-12-20 22:19:07 +0000

    dev - sftp stuff

--- misc/sftp.sh
+++ misc/sftp.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sftp -P 2022 $UNAME@1.studio.boardgamearena.com
+# sftp -P 2022 $UNAME@1.studio.boardgamearena.com
+
+lftp -u $UNAME sftp://1.studio.boardgamearena.com:2022 -e "set sftp:connect-program 'ssh -a -x -i $PATH_TO_SSH';"
+
+# mirror -R --include '(img|modules|^calypso.*|dbmodel.sql|.*\.php|game.*/.json)' --exclude '^(\.git|\.env|\.vscode)(/|$)' . /calypso 
